99d7a3fecadabf1de2901a9837fbab243667c924
mynameisariannao <97466397+mynameisariannao@users.noreply.github.com>
2022-03-01 22:03:11 -0700

    Update craps.js

--- craps.js
+++ craps.js
@@ -48,10 +48,15 @@ function btrCountDownV1(){
     for(var i = 0; i < 11; i++){ // creates a loop to be played
        setTimeout(function(){
         console.log(currTime);
-        document.getElementById("countDownRes").innerHTML = currTime; //presents Blastoff! within the page
+        //display current countdown time with changes caused by admiral's change order 01Mar2022
         if(currTime == 0){
             //Makes 0 into blastoff
             document.getElementById("countDownRes").innerHTML = "Blastoff!";
+        } else if(currTime < 25){ //makes blastoff appear when doing the warning
+            document.getElementById("countDownRes").innerHTML = 
+            "Warning Less than ½ way to launch, time left = " + currTime; 
+        } else {
+            document.getElementById("countDownRes").innerHTML = currTime; //presents Blastoff! within the page
         }
        currTime = currTime - 5;
        }, i * 5000);
