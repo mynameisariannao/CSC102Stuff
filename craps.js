7e68187fe356820cb07ffd27f0da3a6bf1c8b4f7
mynameisariannao <97466397+mynameisariannao@users.noreply.github.com>
2022-03-04 19:57:35 -0700

    Update craps.js

--- craps.js
+++ craps.js
@@ -1,3 +1,56 @@
+function start(){
+    console.log("start() method started");
+    document.getElementById("data").rows["seconds"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["latitude"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["longitude"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["gpsSensor_alt"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["bmpSensor_alt"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["bmpSensor_pres"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["bmpSensor_temp"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["digitalSensor_temp"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["cssSensor_temp"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["cssSensor_eCO2"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["cssSensor_TVOC"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["UV"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["accelX"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["accelY"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["accelZ"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["magneticX"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["magneticY"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["magneticZ"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["gyroX"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["gyroY"].innerHTML = "Reading Data...";
+    document.getElementById("data").rows["gyroZ"].innerHTML = "Reading Data...";
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("stopButton").disabled = false;
+}
+function stop(){
+    console.log("stop() function started");
+    document.getElementById("data").rows["seconds"].innerHTML = "<td>Time Elasped:</td><td>15 Seconds</td>";
+    document.getElementById("data").rows["latitude"].innerHTML = "<td>Latitude:</td><td>0</td>";
+    document.getElementById("data").rows["longitude"].innerHTML = "<td>Longitude:</td><td>0</td>";
+    document.getElementById("data").rows["gpsSensor_alt"].innerHTML = "<td>GPS Altitude:</td><td>0</td>";
+    document.getElementById("data").rows["bmpSensor_alt"].innerHTML = "<td>BMP Sensor Altitude:</td><td>30383.04</td>";
+    document.getElementById("data").rows["bmpSensor_pres"].innerHTML = "<td>BMP Sensor Pressure:</td><td>2.34</td>";
+    document.getElementById("data").rows["bmpSensor_temp"].innerHTML = "<td>BMP Sensor Temperature:</td><td>0</td>";
+    document.getElementById("data").rows["digitalSensor_temp"].innerHTML = "<td>Digital Sensor Temperature:</td><td>24.12</td>";
+    document.getElementById("data").rows["cssSensor_temp"].innerHTML = "<td>CSS Sensor Temperature:</td><td>25</td>";
+    document.getElementById("data").rows["cssSensor_eCO2"].innerHTML = "<td>CSS Sensor eCO2:</td><td>400</td>";
+    document.getElementById("data").rows["cssSensor_TVOC"].innerHTML = "<td>CSS Sensor TVOC:</td><td>0</td>";
+    document.getElementById("data").rows["UV"].innerHTML = "<td>UV:</td><td>0</td>";
+    document.getElementById("data").rows["accelX"].innerHTML = "<td>Accel X:</td><td>-0.87</td>";
+    document.getElementById("data").rows["accelY"].innerHTML = "<td>Accel Y:</td><td>-0.02</td>";
+    document.getElementById("data").rows["accelZ"].innerHTML = "<td>Accel Z:</td><td>9.61</td>";
+    document.getElementById("data").rows["magneticX"].innerHTML = "<td>Magnetic X:</td><td>0.13</td>";
+    document.getElementById("data").rows["magneticY"].innerHTML = "<td>Magnetic Y:</td><td>0.57</td>";
+    document.getElementById("data").rows["magneticZ"].innerHTML = "<td>Magnetic Z:</td><td>-0.24</td>";
+    document.getElementById("data").rows["gyroX"].innerHTML = "<td>Gyro X:</td><td>4.66</td>";
+    document.getElementById("data").rows["gyroY"].innerHTML = "<td>Gyro Y:</td><td>0.01</td>";
+    document.getElementById("data").rows["gyroZ"].innerHTML = "<td>Gyro Z:</td><td>-0.4</td>";
+    document.getElementById("startButton").disabled = false;
+    document.getElementById("stopButton").disabled = true;
+
+}
 var wins = 0;
 var losses = 0;
 var ties = 0;
